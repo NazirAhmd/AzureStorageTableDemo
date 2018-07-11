9418b8fffea94071b1246e5b81c9c775e6a7a877
Palmeto <Palmeto@DESKTOP-F7QDIK3>
2018-07-11 16:46:00 +0530

    Added bulk insert

--- AzureStorageTableDemo/Program.cs
+++ AzureStorageTableDemo/Program.cs
@@ -23,16 +23,48 @@ namespace AzureStorageTableDemo
             CloudTable table = tableClient.GetTableReference("customers");
             table.CreateIfNotExists();
 
-            CustomerEntity customer1 = new CustomerEntity("AP", "A01")
+            //CustomerEntity customer1 = new CustomerEntity("AP", "A01")
+            //{
+            //    FirstName = "Jonny",
+            //    LastName = "Gaddar",
+            //    Email = "jon@wer.com",
+            //    PhoneNumber = "985412547"
+            //};
+
+            //TableOperation insertOPeration = TableOperation.Insert(customer1);
+            //table.Execute(insertOPeration);
+
+            //Bulk Insert
+            TableBatchOperation batchOperation = new TableBatchOperation();
+            CustomerEntity customer2 = new CustomerEntity("AP", "A02")
             {
-                FirstName = "Jonny",
+                FirstName = "Tony",
                 LastName = "Gaddar",
                 Email = "jon@wer.com",
                 PhoneNumber = "985412547"
             };
 
-            TableOperation insertOPeration = TableOperation.Insert(customer1);
-            table.Execute(insertOPeration);
+            CustomerEntity customer3 = new CustomerEntity("AP", "A03")
+            {
+                FirstName = "Golu",
+                LastName = "Gaddar",
+                Email = "jon@wer.com",
+                PhoneNumber = "985412547"
+            };
+
+            CustomerEntity customer4 = new CustomerEntity("AP", "A04")
+            {
+                FirstName = "Molu",
+                LastName = "Gaddar",
+                Email = "jon@wer.com",
+                PhoneNumber = "985412547"
+            };
+
+            batchOperation.Insert(customer2);
+            batchOperation.Insert(customer3);
+            batchOperation.Insert(customer4);
+
+            table.ExecuteBatch(batchOperation);
         }
     }
 
